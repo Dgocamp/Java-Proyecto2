8275a2f513ebd4498f43fdb419070aa7a27c3b08
DaniloAraneda123 <daes1128@gmail.com>
2018-05-09 21:35:03 -0300

    prueba de Test

---
+++ vista/Testeador.java
@@ -0,0 +1,10 @@
+package vista;
+
+public class Testeador 
+{
+    public int[] generador(int numeroElementos, int magnitud)
+    {
+        int []numerosGenerados= new int[numeroElementos];
+        return numerosGenerados;
+    }
+}
